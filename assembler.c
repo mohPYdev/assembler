23cac9366a9cf91afec91779acb706bf7be48187
Masoud <msmsd778@gmail.com>
2021-04-02 11:57:29 +0430

    added mnemonic and oppcodes

--- assembler.c
+++ assembler.c
@@ -34,6 +34,9 @@ void writeToFile(){
 
 int main()
 {
-    printf("Hello, World!\n");
+    const char *mnemonics[15] = {"add","sub","slt","or","nand","addi",
+    "slti","ori","lui","lw","sw","beq","jalr","j","halt"};
+    const int oppcodes[15] = {0b0000,0b0001,0b0010,0b0011,0b0100,0b0101,0b0110,
+    0b0111,0b1000,0b1001,0b1010,0b1011,0b1100,0b1101,0b1110};
     return 0;
 }
