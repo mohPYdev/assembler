a8e93eeb6a5ad35a37bfa331ec4b7e9b6516a38a
mohamad <rafieiyan.moh@gmail.com>
2021-04-02 10:21:44 +0430

    initial commit and base functions

---
+++ assembler.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+void readFile()
+{
+
+}
+
+void createLabelAddress()
+{
+
+}
+
+char* instType()
+{
+
+}
+
+void builtFormat()
+{
+
+}
+
+void coovertToDec(){
+
+}
+
+void writeToFile(){
+
+}
+
+
+
+
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
