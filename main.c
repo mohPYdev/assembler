137331f424c85d4f820e8e54e76bce41cf002b65
mohamad <rafieiyan.moh@gmail.com>
2021-04-04 13:34:12 +0430

    created the symbol table

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 void createLabelAddress()
 {
@@ -30,7 +31,7 @@ void writeToFile(){
 
 int main()
 {
-    const char *mnemonics[15] = {"add","sub","slt","or","nand","addi",
+    const char *mnemonics[16] = {"add","sub","slt","or","nand","addi",
     "slti","ori","lui","lw","sw","beq","jalr","j","halt"};
     const int oppcodes[15] = {0b0000,0b0001,0b0010,0b0011,0b0100,0b0101,0b0110,
     0b0111,0b1000,0b1001,0b1010,0b1011,0b1100,0b1101,0b1110};
@@ -39,29 +40,36 @@ int main()
     file = fopen("program.as", "r");
     char line[200];
     char firstWord[6];
+    char labels[10][6];
+    char addresses[10];
+    int lineCounter = 0;
+    int z = 0;
     if (file) {
-        while (fgets(line, sizeof(line), file)) {
+        while (fgets(line, sizeof(line), file)) {  
+            firstWord[0] = '\0';
             int i = 0;
-            int z = 0;
             while(line[i] != '\t'){
                 strncat(firstWord , &line[i] , 1);
                 i++;
             }
-            //printf("%s",firstWord);
-            char* labels[10];
+            // printf("%s \n", firstWord);
+            bool inThere = false;
             for(int j = 0 ; j < 15 ; j++){
                 int x = strcmp(firstWord,mnemonics[j]);
                 if(x == 0){
+                    inThere = true;
                     break;
                 }
-                else if(j == 14){
-                    strcpy(labels[z],firstWord);
-                    z++;
-                }
-                else
-                    continue;
             }
-            printf("\n");
+
+
+            if (inThere == false){
+                strcpy(labels[z] , firstWord);
+                addresses[z] = lineCounter;
+                z++;  
+            }   
+
+            lineCounter++;              
         }
         fclose(file);
     }
--- program.as
+++ program.as
@@ -1,8 +1,8 @@
 lw	1,0,five	#load reg1 with 5(symbolic address)
 lw	2,1,2		#load reg2 with -1 (numeric address)
 start	add     1,1,2       	#decrement reg1
-beq 	0,1,done	#goto end of program when reg1==0
-j	start	    	#go back tto the beginning of the loop
+beq	0,1,done	#goto end of program when reg1==0
+j	start	    #go back tto the beginning of the loop
 done	halt		    	#end of program
 five	.fill	5
 neg1	.fill	-1
